e9dfd867ba9781bc5d1933bb81430920811193d8
DH <psycn@naver.com>
2024-12-21 00:32:33 +0900

    h2, jpa적용

--- user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.example.user_service.controller;
 
+import com.example.user_service.dto.UserDto;
+import com.example.user_service.service.UserService;
 import com.example.user_service.vo.Greeting;
+import com.example.user_service.vo.RequestUser;
+import com.example.user_service.vo.ResponseUser;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/")
 public class UserController {
+
+    private final UserService userService;
 
     @Autowired
     private Greeting greeting;
@@ -22,5 +32,18 @@ public class UserController {
     @GetMapping("/welcome")
     public String welcome() {
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
 }
