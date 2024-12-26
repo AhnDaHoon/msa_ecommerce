5dd634d5a7aa3b82380dac121ed99734d24a3be2
psycn <psycn@itoplevel.com>
2024-12-26 20:44:48 +0900

    security적용

--- user-service/src/main/java/com/example/user_service/security/WebSecurity.java
+++ user-service/src/main/java/com/example/user_service/security/WebSecurity.java
@@ -1,25 +1,56 @@
 package com.example.user_service.security;
 
+import com.example.user_service.service.UserService;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private UserService userService;
+
+    private Environment evn;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
+    public WebSecurity(Environment evn, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.evn = evn;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                        .hasIpAddress("127.0.0.1")
+                        .and()
+                        .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception{
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
